26fd2ff742994929551eafcf14f68da971b25213
Fernando <fernandowll@hotmail.com>
2019-07-20 15:49:36 -0300

    Primeiro Commit LojaApp

---
+++ src/MVC/View.php
@@ -0,0 +1,13 @@
+<?php
+namespace LojaApp\MVC;
+
+
+
+class View
+{
+    public static function render($view, $data = [])
+    {  
+        extract($data);
+        require TEMPLATES . '/' . $view . '.phtml';
+    }
+}
